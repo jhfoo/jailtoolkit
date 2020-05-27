4ef37f66f7bb3e10998f1677daed15c1eaa54d5c
Foo JH <jhfoo@jill.kungfoo.home>
2020-05-27 02:00:02 -0700

    UPDATED: config file for createjail

--- bin/createjail.sh
+++ bin/createjail.sh
@@ -1,13 +1,16 @@
+BASEDIR=~/jailtoolkit
+. $BASEDIR/conf/createjail.conf
+
 SIOCAGE='sudo iocage'
-$SIOCAGE create -r 12.1-RELEASE -n $1
+$SIOCAGE create -r $JAIL_RELEASE -n $1
 $SIOCAGE set bpf=1 $1
 $SIOCAGE set vnet=1 $1
 $SIOCAGE set dhcp=1 $1
 $SIOCAGE start $1
 
 # install pkgs
-$SIOCAGE exec $1 "pkg install -y zsh git"
+$SIOCAGE exec $1 "pkg install -y ${INSTALL_PACKAGES}"
 
 # create app account
-$SIOCAGE exec $1 "echo abcdef | pw useradd app -h 0 -m"
-$SIOCAGE exec $1 "touch /home/app/.zshrc"
+$SIOCAGE exec $1 "echo ${APP_PWD} | pw useradd ${APP_ID} -h 0 -m -s /usr/local/bin/zsh"
+$SIOCAGE exec $1 "touch /home/${APP_ID}/.zshrc"
