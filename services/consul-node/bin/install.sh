778cbb56a52d1b9eec79013ef3d474f440d12d5f
Foo JH <jhfoo@jill.kungfoo.home>
2020-05-28 23:45:19 -0700

    UPDATED: dynamic consul config install (incomplete)

--- services/consul-node/bin/install.sh
+++ services/consul-node/bin/install.sh
@@ -1,12 +1,9 @@
-install()
-{
-}
-
 if [ -z "$1" ]
 then
   echo ERROR: Missing argument for jail template
 else
   siocage="sudo iocage"
+  $siocage exec $1 "rm -rf ./jailtoolkit"
   $siocage exec $1 "git clone https://github.com/jhfoo/jailtoolkit.git"
   # jail to exec install script
   $siocage exec $1 "./jailtoolkit/services/consul-node/bin/localinstall.sh"
