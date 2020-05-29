d8d05411a07b8be71ed2ff79486c7444acb3fea7
Foo JH <jhfoo@jill.kungfoo.home>
2020-05-28 23:34:14 -0700

    dynamic consul config install (incomplete)

--- services/consul-node/bin/install.sh
+++ services/consul-node/bin/install.sh
@@ -1,21 +1,15 @@
-#!/usr/local/bin/zsh
+install()
+{
+  siocage="sudo iocage"
+  $siocage exec $1 "git clone https://github.com/jhfoo/jailtoolkit.git"
+  # jail to exec install script
+  $siocage exec $1 "./jailtoolkit/services/consul-node/bin/localinstall.sh"
+}
 
-if [ -z "$1" ] 
+if [ -z "$1" ]
 then
   echo ERROR: Missing argument for jail template
 else
-  main
+  install
 fi
 
-test()
-{
-  echo "Test works"
-  ls -l
-}
-
-main()
-{
-  siocage="sudo iocage"
-  $siocage exec $1 "git clone https://github.com/jhfoo/consulinstall.git"
-  $siocage exec $1 "./consulinstall/bin/install.sh"
-}
---
+++ services/consul-node/bin/localinstall.sh
@@ -0,0 +1,34 @@
+INSTALL_DIR=/jailtoolkit/services/consul-node
+CONSUL_CONF_DIR=/usr/local/etc/consul.d
+export HOSTNAME=`hostname -s`
+
+# If consul is already installed and running
+service consul stop
+
+echo *** Installing consul pkg ***
+pkg install -y consul perl5
+
+echo *** Deploying basic config ***
+# create conf dir
+mkdir ${CONSUL_CONF_DIR}
+chown consul:consul ${CONSUL_CONF_DIR}
+
+# install templated configs
+TEMPLATE_REPLACE="perl -pe 's;(\\*)(\$([a-zA-Z_][a-zA-Z_0-9]*)|\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})?;substr($1,0,int(length($1)/2)).($2&&length($1)%2?$2:$ENV{$3||$4});eg'"
+TARGET="${INSTALL_DIR}/conf/node.json.template"
+DEST="${CONSUL_CONF_DIR}/node.json"
+#$TEMPLATE_REPLACE $TARGET 
+perl -pe 's;(\\*)(\$([a-zA-Z_][a-zA-Z_0-9]*)|\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})?;substr($1,0,int(length($1)/2)).($2&&length($1)%2?$2:$ENV{$3||$4});eg' $TARGET > $DEST
+rm $TARGET
+
+TARGET="${INSTALL_DIR}/conf/*"
+cp $TARGET /usr/local/etc/consul.d/
+
+# Enable Consul to log in /var/log/consul
+mkdir -p /var/log/consul
+chown consul:consul /var/log/consul
+
+
+# Start it up!
+sysrc consul_enable="YES"
+service consul start
