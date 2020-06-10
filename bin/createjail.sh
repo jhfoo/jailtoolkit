4c4534c1f61c29b3a0b3edabed4008a2975322ed
Foo JH <jhfoo@jill.kungfoo.home>
2020-06-10 03:43:00 -0700

    NEW: -nobridge option for createjail.sh

--- bin/createjail.sh
+++ bin/createjail.sh
@@ -1,11 +1,38 @@
 BASEDIR=~/jailtoolkit
+
+# load configurable params
 . $BASEDIR/conf/createjail.conf
+
+# parse options
+OPT_NOBRIDGE=0
+
+while [ -n "$1" ]
+do
+  case "$1" in
+  -nobridge)
+    echo "[Option] Remove $IF_HOST from bridge $IF_BRIDGE"
+    OPT_NOBRIDGE=1
+    shift ;;
+  -y)
+    echo "-y"
+    shift ;;
+  *)
+    break ;;
+  esac
+done
 
 SIOCAGE='sudo iocage'
 $SIOCAGE create -r $JAIL_RELEASE -n $1
 $SIOCAGE set bpf=1 $1
 $SIOCAGE set vnet=1 $1
 $SIOCAGE set dhcp=1 $1
+
+if [ $OPT_NOBRIDGE -eq 1 ]
+then 
+  echo Removing host interface from bridge
+  sudo ifconfig $IF_BRIDGE deletem $IF_HOST
+fi
+
 $SIOCAGE start $1
 
 # install pkgs
