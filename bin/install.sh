85012ae26409798d280fba7ced3eee62ef87ee24
Foo JH <jhfoo@jill.kungfoo.home>
2020-06-10 21:31:10 -0700

    UPDATED: removes relogin for updated env settings

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,5 @@
-echo -e "\nexport PATH=\$PATH:~/jailtoolkit/bin\n" >> ~/.zshrc
-echo -e "\nalias siocage='sudo iocage'\n" >> ~/.zshrc
+echo -e "\nexport PATH=\$PATH:~/jailtoolkit/bin" >> ~/.zshrc
+PATH=$PATH:~/jailtoolkit/bin
+echo -e "alias siocage='sudo iocage'" >> ~/.zshrc
+alias siocage='sudo iocage'
 sudo pkg install -y py37-iocage
